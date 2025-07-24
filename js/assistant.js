3998ed7e291446ace46983abd97202b73516fbb4
Antonio Moneo <antoniomoneo@users.noreply.github.com>
2025-07-24 14:05:54 +0200

    Bump plugin version to 2.9.25

--- js/assistant.js
+++ js/assistant.js
@@ -1,2 +1,16 @@
-// Admin JS for OpenAI Assistant v2.9.19
-jQuery(()=>console.log('OA Admin loaded v2.9.19'));
+// Admin JS for OpenAI Assistant v2.9.25
+jQuery(function($){
+  console.log('OA Admin loaded v2.9.25');
+  $('#oa-add-config').on('click', function(){
+    var table = $('#oa-configs tbody');
+    var idx = table.find('tr').length;
+    var row = '<tr>'+
+      '<td><input class="regular-text" name="oa_assistant_configs['+idx+'][nombre]" placeholder="Ej: Soporte" /></td>'+
+      '<td><input class="regular-text" name="oa_assistant_configs['+idx+'][slug]" placeholder="soporte" /></td>'+
+      '<td><input class="regular-text" name="oa_assistant_configs['+idx+'][assistant_id]" placeholder="asst_..." /></td>'+
+      '<td><textarea name="oa_assistant_configs['+idx+'][developer_instructions]" rows="3" style="width:100%;" placeholder="Instrucciones para el assistant"></textarea></td>'+
+      '<td><input class="regular-text" name="oa_assistant_configs['+idx+'][vector_store_id]" placeholder="categoria" /></td>'+
+    '</tr>';
+    table.append(row);
+  });
+});
